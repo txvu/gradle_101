cfb637c7aca1c81809033c3def5a21377c0df815
Trinh Vu <78838605+txvu100@users.noreply.github.com>
2023-07-24 16:41:35 -0500

    Add files via upload

---
+++ bluebox/projectA/settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "projectA"
+
+println("This is executed during the initialization phase.")
+
+include("projectB")
---
+++ bluebox/projectB/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "projectB"
+println("This is executed during the initialization phase.")
